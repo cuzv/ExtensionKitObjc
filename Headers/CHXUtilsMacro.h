0edb63bbbb5f672ee604ad15992b4094d4b5ab9d
atcuan <atcuan@gmail.com>
2015-05-04 20:58:11 +0800

    update

--- Headers/CHXUtilsMacro.h
+++ Headers/CHXUtilsMacro.h
@@ -9,6 +9,8 @@
 #ifndef Haioo_CHXUtilsMacro_h
 #define Haioo_CHXUtilsMacro_h
 
+#define iOS6 (NSFoundationVersionNumber > NSFoundationVersionNumber_iOS_5_1)
+#define iOS7 (NSFoundationVersionNumber > NSFoundationVersionNumber_iOS_6_1)
 #define iOS8 (NSFoundationVersionNumber > NSFoundationVersionNumber_iOS_7_1)
 
 #endif
--- WildAppExtension.h
+++ WildAppExtension.h
@@ -14,6 +14,7 @@
 #import "CHXFunctionDefine.h"
 #import "CHXFontDefine.h"
 #import "CHXColorDefine.h"
+#import "CHXUtilsMacro.h"
 
 // Categories
 #import "CAAnimationExtension.h"
