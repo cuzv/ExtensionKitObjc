da1d96d93f8ad74486ea464eb73d000c6cc52cd6
Moch Xiao <cuzv@qq.com>
2015-08-22 11:05:45 +0800

    add code from WWDC14 AdvancedCollectionView

--- Categories/UIResponderExtension/UIResponder+CHXAddition.m
+++ Categories/UIResponderExtension/UIResponder+CHXAddition.m
@@ -20,7 +20,21 @@
     }
     
     return nil;
-
 }
+
+- (BOOL)chx_sendAction:(SEL)action from:(id)sender {
+    // Get the target in the responder chain
+    id target = sender;
+    
+    while (target && ![target canPerformAction:action withSender:sender]) {
+        target = [target nextResponder];
+    }
+    
+    if (!target)
+        return NO;
+    
+    return [[UIApplication sharedApplication] sendAction:action to:target from:sender forEvent:nil];
+}
+
 
 @end
